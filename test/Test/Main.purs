93778b93abf0b2f123361b2a273c5d247e824e92
matoruru <matoruru@shgt.jp>
2021-03-23 23:15:00 +0900

    Add tests

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -6,7 +6,6 @@ import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Test.Spec.Reporter (consoleReporter)
 import Test.Spec.Runner (runSpec)
-import Test.Survey.IntegrageOperations (integrateOperations) as Test
 import Test.Survey.Operation (operations) as Test
 import Test.Survey.QuestionItem (questionItems) as Test
 import Test.Survey.Util (utils) as Test
@@ -14,6 +13,5 @@ import Test.Survey.Util (utils) as Test
 main :: Effect Unit
 main = launchAff_ $ runSpec [consoleReporter] do
   Test.operations
-  Test.integrateOperations
   Test.questionItems
   Test.utils
--- test/Test/Survey/Operation.purs
+++ test/Test/Survey/Operation.purs
@@ -4,34 +4,58 @@ import Prelude
 
 import Ansi.Codes (EscapeCode(..))
 import Control.Monad.Error.Class (class MonadThrow)
+import Data.Array ((..))
+import Data.Foldable (foldl)
 import Data.Newtype (wrap)
 import Data.Show (class Show)
 import Effect.Exception (Error)
 import Survey.Operation (Operation(..), evalOperation)
+import Survey.Type (OutputState)
 import Test.Spec (SpecT, describe, it)
 import Test.Spec.Assertions (fail, shouldEqual)
+import Test.TestUtil (composeOperations)
 
 operations :: forall t1 t2. Monad t1 => MonadThrow Error t2 => SpecT t2 Unit t1 Unit
-operations = do
+operations = describe "Composing multiple operations" do
   describe "Delete a character under the cursor" do
     it "Delete the last character" do
-      evalOperation DeleteUnderCursor
-        { cursorPosition: 5, plainText: "abcde", escapes: [ wrap $ Back 1 ] } `shouldEqual`
+      composeOperations
+        [ MoveLeft
+        , DeleteUnderCursor
+        ]
+        { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
         { cursorPosition: 5, plainText: "abcd", escapes: [] }
 
     it "Delete before the last character" do
-      evalOperation DeleteUnderCursor
-        { cursorPosition: 4, plainText: "abcde", escapes: [ wrap $ Back 2 ] } `shouldEqual`
+      composeOperations
+        [ MoveLeft
+        , MoveLeft
+        , DeleteUnderCursor
+        ]
+        { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
         { cursorPosition: 4, plainText: "abce", escapes: [ wrap $ Back 1 ] }
 
     it "Delete the first character" do
-      evalOperation DeleteUnderCursor
-        { cursorPosition: 1, plainText: "abcde", escapes: [ wrap $ Back 5 ] } `shouldEqual`
+      composeOperations
+        [ MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , DeleteUnderCursor
+        ]
+        { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
         { cursorPosition: 1, plainText: "bcde", escapes: [ wrap $ Back 4 ] }
 
     it "Delete after the first character" do
-      evalOperation DeleteUnderCursor
-        { cursorPosition: 2, plainText: "abcde", escapes: [ wrap $ Back 4 ] } `shouldEqual`
+      composeOperations
+        [ MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , DeleteUnderCursor
+        ]
+        { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
         { cursorPosition: 2, plainText: "acde", escapes: [ wrap $ Back 3 ] }
 
     it "Deleting at the end of line shouldn't do anything" do
@@ -46,39 +70,75 @@ operations = do
         { cursorPosition: 5, plainText: "abcd", escapes: [] }
 
     it "Delete before the last character" do
-      evalOperation DeleteBackward
-        { cursorPosition: 5, plainText: "abcde", escapes: [] } `shouldEqual`
-        { cursorPosition: 4, plainText: "abce", escapes: [] }
+      composeOperations
+        [ MoveLeft
+        , DeleteBackward
+        ]
+        { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
+        { cursorPosition: 4, plainText: "abce", escapes: [ wrap $ Back 1 ] }
 
     it "Delete the first character" do
-      evalOperation DeleteBackward
-        { cursorPosition: 2, plainText: "abcde", escapes: [] } `shouldEqual`
-        { cursorPosition: 1, plainText: "bcde", escapes: [] }
+      composeOperations
+        [ MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , DeleteBackward
+        ]
+        { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
+        { cursorPosition: 1, plainText: "bcde", escapes: [ wrap $ Back 4 ] }
 
     it "Delete after the first character" do
-      evalOperation DeleteBackward
-        { cursorPosition: 3, plainText: "abcde", escapes: [] } `shouldEqual`
-        { cursorPosition: 2, plainText: "acde", escapes: [] }
+      composeOperations
+        [ MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , DeleteBackward
+        ]
+        { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
+        { cursorPosition: 2, plainText: "acde", escapes: [ wrap $ Back 3 ] }
 
-    it "Deleting backward at 1 shoudn't do anything" do
-      evalOperation DeleteBackward
-        { cursorPosition: 1, plainText: "abcde", escapes: [] } `shouldEqual`
-        { cursorPosition: 1, plainText: "abcde", escapes: [] }
+    it "Deleting backward at 1 should do nothing" do
+      composeOperations
+        [ MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , DeleteBackward
+        ]
+        { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
+        { cursorPosition: 1, plainText: "abcde", escapes: [ wrap $ Back 5 ] }
 
   describe "Delete from under the cursor to the end of line" do
     it "Delete the c to the end" do
-      evalOperation DeleteUnderCursorToTail
-        { cursorPosition: 3, plainText: "abcde", escapes: [ wrap $ Back 3 ] } `shouldEqual`
+      composeOperations
+        [ MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , DeleteUnderCursorToTail
+        ]
+        { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
         { cursorPosition: 3, plainText: "ab", escapes: [] }
 
     it "Delete from the head" do
-      evalOperation DeleteUnderCursorToTail
-        { cursorPosition: 1, plainText: "abcde", escapes: [ wrap $ Back 5 ] } `shouldEqual`
+      composeOperations
+        [ MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , DeleteUnderCursorToTail
+        ]
+        { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
         { cursorPosition: 1, plainText: "", escapes: [] }
 
     it "Deleting the last character" do
-      evalOperation DeleteUnderCursorToTail
-        { cursorPosition: 5, plainText: "abcde", escapes: [ wrap $ Back 1 ] } `shouldEqual`
+      composeOperations
+        [ MoveLeft
+        , DeleteUnderCursorToTail
+        ]
+        { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
         { cursorPosition: 5, plainText: "abcd", escapes: [] }
 
     it "Deleting from the end shoudn't do anything" do
@@ -86,11 +146,152 @@ operations = do
         { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
         { cursorPosition: 6, plainText: "abcde", escapes: [] }
 
-  describe "DeleteBeforeUnderCursorToHead" do
-    it "" $ fail "TODO"
+  describe "Delete from before the cursor to the head of line" do
+    it "Delete the first character" do
+      composeOperations
+        [ MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , DeleteBeforeUnderCursorToHead
+        ]
+        { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
+        { cursorPosition: 1, plainText: "bcde", escapes: [ wrap $ Back 4 ] }
+
+    it "Delete the first two characters" do
+      composeOperations
+        [ MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , DeleteBeforeUnderCursorToHead
+        ]
+        { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
+        { cursorPosition: 1, plainText: "cde", escapes: [ wrap $ Back 3 ] }
+
+    it "Delete from the end of line (delete all characters)" do
+      composeOperations
+        [ DeleteBeforeUnderCursorToHead
+        ]
+        { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
+        { cursorPosition: 1, plainText: "", escapes: [] }
+
+    it "Delete from the head shoudn't do anything" do
+      composeOperations
+        [ MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , DeleteBeforeUnderCursorToHead
+        ]
+        { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
+        { cursorPosition: 1, plainText: "abcde", escapes: [ wrap $ Back 5 ] }
 
   describe "DeleteAWordBeforeCursor" do
-    it "" $ fail "TODO"
+    it "Delete a first word before cursor from just after the word" do
+      composeOperations
+        [ DeleteAWordBeforeCursor
+        ]
+        { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
+        { cursorPosition: 1, plainText: "", escapes: [] }
+
+    it "Delete a first word before cursor from after some spaces" do
+      composeOperations
+        [ DeleteAWordBeforeCursor
+        ]
+        { cursorPosition: 7, plainText: "abcde ", escapes: [] } `shouldEqual`
+        { cursorPosition: 1, plainText: "", escapes: [] }
+
+    it "Delete a first word before cursor from after some spaces" do
+      composeOperations
+        [ DeleteAWordBeforeCursor
+        ]
+        { cursorPosition: 8, plainText: "abcde  ", escapes: [] } `shouldEqual`
+        { cursorPosition: 1, plainText: "", escapes: [] }
+
+    it "Delete a first word before cursor from after some spaces" do
+      composeOperations
+        [ DeleteAWordBeforeCursor
+        ]
+        { cursorPosition: 9, plainText: "abcde   ", escapes: [] } `shouldEqual`
+        { cursorPosition: 1, plainText: "", escapes: [] }
+
+    it "Delete a first word before cursor from after some spaces" do
+      composeOperations
+        [ DeleteAWordBeforeCursor
+        ]
+        { cursorPosition: 8, plainText: "abcde   ", escapes: [] } `shouldEqual`
+        { cursorPosition: 1, plainText: " ", escapes: [ wrap $ Back 1 ] }
+
+    it "Delete a first word before cursor from inside the word" do
+      composeOperations
+        [ MoveLeft
+        , MoveLeft
+        , DeleteAWordBeforeCursor
+        ]
+        { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
+        { cursorPosition: 1, plainText: "de", escapes: [ wrap $ Back 2 ] }
+
+    it "Deleting a first word from the head shoudn't do anything" do
+      composeOperations
+        [ MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , DeleteAWordBeforeCursor
+        ]
+        { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
+        { cursorPosition: 1, plainText: "abcde", escapes: [ wrap $ Back 5 ] }
+
+    it "Delete a word after some words" do
+      composeOperations
+        [ DeleteAWordBeforeCursor
+        ]
+        { cursorPosition: 10, plainText: "abcde abc", escapes: [] } `shouldEqual`
+        { cursorPosition: 7, plainText: "abcde ", escapes: [] }
+
+    it "Delete a word after some words" do
+      composeOperations
+        [ DeleteAWordBeforeCursor
+        ]
+        { cursorPosition: 11, plainText: "abcde  abc", escapes: [] } `shouldEqual`
+        { cursorPosition: 8, plainText: "abcde  ", escapes: [] }
+
+    it "Delete a word separated with \"/\"" do
+      composeOperations
+        [ DeleteAWordBeforeCursor
+        ]
+        { cursorPosition: 10, plainText: "abcde/abc", escapes: [] } `shouldEqual`
+        { cursorPosition: 7, plainText: "abcde/", escapes: [] }
+
+    it "Delete a word separated with \"/\" from inside the word" do
+      composeOperations
+        [ DeleteAWordBeforeCursor
+        ]
+        { cursorPosition: 8, plainText: "abcde/abc", escapes: [] } `shouldEqual`
+        { cursorPosition: 8, plainText: "abcde/bc", escapes: [ wrap $ Back 2 ] }
+
+    it "Delete the first \"/\"" do
+      composeOperations
+        [ DeleteAWordBeforeCursor
+        ]
+        { cursorPosition: 2, plainText: "/", escapes: [] } `shouldEqual`
+        { cursorPosition: 1, plainText: "", escapes: [] }
+
+    it "Delete the first \"/\" spaces" do
+      composeOperations
+        [ DeleteAWordBeforeCursor
+        ]
+        { cursorPosition: 4, plainText: "/  ", escapes: [] } `shouldEqual`
+        { cursorPosition: 1, plainText: "", escapes: [] }
+
+    it "Delete the first \"/\" spaces" do
+      composeOperations
+        [ DeleteAWordBeforeCursor
+        ]
+        { cursorPosition: 5, plainText: " /  ", escapes: [] } `shouldEqual`
+        { cursorPosition: 2, plainText: " ", escapes: [] }
 
   describe "Print a character from the cursor position" do
     it "Print a character from the end" do
@@ -99,50 +300,68 @@ operations = do
         { cursorPosition: 7, plainText: "abcdef", escapes: [] }
 
     it "Print a character before the last character" do
-      evalOperation (PrintCharacter "c")
-        { cursorPosition: 5, plainText: "abcde", escapes: [] } `shouldEqual`
-        { cursorPosition: 6, plainText: "abcdce", escapes: [] }
+      composeOperations
+        [ MoveLeft
+        , (PrintCharacter "c")
+        ]
+        { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
+        { cursorPosition: 6, plainText: "abcdce", escapes: [ wrap $ Back 1 ] }
 
     it "Print a character from the beginning" do
-      evalOperation (PrintCharacter "z")
-        { cursorPosition: 1, plainText: "abcde", escapes: [] } `shouldEqual`
-        { cursorPosition: 2, plainText: "zabcde", escapes: [] }
+      composeOperations
+        [ MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , (PrintCharacter "z")
+        ]
+        { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
+        { cursorPosition: 2, plainText: "zabcde", escapes: [ wrap $ Back 5 ] }
 
     it "Print a character after the first character" do
-      evalOperation (PrintCharacter "z")
-        { cursorPosition: 2, plainText: "abcde", escapes: [] } `shouldEqual`
-        { cursorPosition: 3, plainText: "azbcde", escapes: [] }
+      composeOperations
+        [ MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , (PrintCharacter "z")
+        ]
+        { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
+        { cursorPosition: 3, plainText: "azbcde", escapes: [ wrap $ Back 4 ] }
 
-  describe "Move the cursor" do
-    it "Move right, d to e" do
-      evalOperation MoveRight
-        { cursorPosition: 4, plainText: "abcde", escapes: [] } `shouldEqual`
-        { cursorPosition: 5, plainText: "abcde", escapes: [ wrap $ Forward 1 ] }
-
-    it "Move right, e to the tail" do
-      evalOperation MoveRight
-        { cursorPosition: 5, plainText: "abcde", escapes: [] } `shouldEqual`
-        { cursorPosition: 6, plainText: "abcde", escapes: [ wrap $ Forward 1 ] }
-
-    it "Cannot move right from the tail" do
-      evalOperation MoveRight
+  describe "The cursor movements should be aggregated" do
+    it "Move left, left, right, and right = []" do
+      composeOperations
+        [ MoveLeft
+        , MoveLeft
+        , MoveRight
+        , MoveRight
+        ]
         { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
         { cursorPosition: 6, plainText: "abcde", escapes: [] }
 
-    it "Move left, c to b" do
-      evalOperation MoveLeft
-        { cursorPosition: 3, plainText: "abcde", escapes: [] } `shouldEqual`
-        { cursorPosition: 2, plainText: "abcde", escapes: [ wrap $ Back 1 ] }
+    it "Move left, left, left, left and left = [Back 5]" do
+      composeOperations
+        [ MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , MoveLeft
+        , MoveLeft
+        ]
+        { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
+        { cursorPosition: 1, plainText: "abcde", escapes: [ wrap $ Back 5 ] }
 
-    it "Move left, b to a (head)" do
-      evalOperation MoveLeft
-        { cursorPosition: 2, plainText: "abcde", escapes: [] } `shouldEqual`
-        { cursorPosition: 1, plainText: "abcde", escapes: [ wrap $ Back 1 ] }
-
-    it "Cannot move left from the head" do
-      evalOperation MoveLeft
-        { cursorPosition: 1, plainText: "abcde", escapes: [] } `shouldEqual`
-        { cursorPosition: 1, plainText: "abcde", escapes: [] }
+    it "Move left, left, right, right and left = [Back 1]" do
+      composeOperations
+        [ MoveLeft
+        , MoveLeft
+        , MoveRight
+        , MoveRight
+        , MoveLeft
+        ]
+        { cursorPosition: 6, plainText: "abcde", escapes: [] } `shouldEqual`
+        { cursorPosition: 5, plainText: "abcde", escapes: [ wrap $ Back 1 ] }
 
   describe "MoveToTail" do
     it "" $ fail "TODO"
